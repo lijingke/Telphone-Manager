a6ca006d6f38eea232d698dcf5d4fbb085db0696
HuJunYuan <hjyhujunyuan@hotmail.com>
2016-10-27 13:33:58 +0800

    去掉了一些多余的组件和文件

---
+++ Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using pdm;
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        pdm.Model.Users user = new pdm.Model.Users();
+        user.Dealer_Name = "asjdlk";
+        user.Dealer_Psw = "jdosja";
+
+        new pdm.BLL.Users().Add(user);
+    }
+}
